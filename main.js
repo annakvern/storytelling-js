4d0176151389a7c1ace45d29daf266ea67390aa8
Anna <anna.kvernplassen@gmail.com>
2024-10-30 13:20:25 +0100

    create first function

--- main.js
+++ main.js
@@ -5,22 +5,32 @@ const power = "";
 
 /** This is the starting point for the program */
 function main() {
-  // setupEventListeners();
+  setupEventListeners();
   // startClock();
   // fetchUsersFromDatabase()
 }
 
 /** Register all events for buttons and other ui elements */
 function setupEventListeners() {
-  //burgerMenu.onclick = toggleHeaderOpen;
+  firstOption.onclick = storyBricksStart;
+  secondOption.style.display = 'none';
+  storyText.style.display = 'none';
   //zoom.onclick = toggleZoom;
   // sätt upp fler knappar så dom klickbara.
 }
 
+/** Displays the starting story with choices in the story */
 function storyBricksStart() {
-  
-  // firstOption -> storyDragonflyPower()
-  // secondOption -> storyGoblinPrank()
+  welcomeTitle.style.display = 'none';
+  storyText.innerText = "You enter the garden on laid out bricks. You need to take large steps over the cracks between the bricks not to fall. It's a sunny and warm day with a light breeze. Enjoyable for most creatures, you see buzzing bees collecting nectar on the flowers, hard-working ants carrying material for their home, and oh - what's that? You think you saw something furry in the shadows behind the bush to the right.";
+  storyText.style.display = 'block';
+  welcome.style.display = 'none';
+  firstOption.innerHTML = 'Stay in the sun and go smell the roses ahead';
+  secondOption.innerHTML = 'Go look for the furry thing you saw';
+  firstOption.onclick = storyDragonflyPower;
+  secondOption.onclick = storyGoblinPrank;
+  secondOption.style.display = 'block';
+  storyImg.src = 'assets/images/enter.jpg';
 }
 
 function storyDragonflyPower() {
